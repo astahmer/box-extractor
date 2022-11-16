4d2fa2a86c96f611fa5fdeab3b9da094471b9e28
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-11-17 00:39:18 +0100

    chore: simplify theme for easier testing

--- demo/src/theme/DessertBox.tsx
+++
@@ -1,8 +0,0 @@
-import { createBox } from "@dessert-box/react";
-import type { ComponentProps } from "react";
-
-import { themeSprinkles } from "./sprinkles.css";
-
-export const DessertBox = createBox({ atoms: themeSprinkles });
-
-export type BoxProps = ComponentProps<typeof DessertBox>;
