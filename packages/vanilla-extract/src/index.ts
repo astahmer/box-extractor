356c1aa88d46df10b73d8349f59c6d4a8a9c34eb
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-12-21 10:49:35 +0100

    fix: re-use vanillaExtractPlugin directly from fork chore: rm createBoxSprinklesInternal from exposed fns

--- packages/vanilla-extract/src/index.ts
+++ packages/vanilla-extract/src/index.ts
@@ -1,4 +1,3 @@
 export { createBoxSprinkles } from "./createBoxSprinkles";
 export type { AnySprinklesFn, SprinkleConditionsNames } from "./createBoxSprinklesInternal";
-export { createBoxSprinklesInternal } from "./createBoxSprinklesInternal";
 export { getBoxProps } from "./getBoxProps";
