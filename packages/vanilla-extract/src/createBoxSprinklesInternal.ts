921e8e3e190cf7da00c21e68eced9b553961f468
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-12-23 10:58:53 +0100

    fix(ve-adapter): typings createBoxSprinklesInternal

--- packages/vanilla-extract/src/createBoxSprinklesInternal.ts
+++ packages/vanilla-extract/src/createBoxSprinklesInternal.ts
@@ -1,11 +1,14 @@
 import { createSprinkles } from "@vanilla-extract/sprinkles";
 import { getSprinklesConfig } from "./getSprinklesConfig";
-import type { SprinklesFn } from "./sprinkle-types";
+import type { SprinklesConditions, SprinklesFn } from "./sprinkle-types";
 import type { SprinklesProperties } from "./types";
 
 export const createBoxSprinklesInternal = <Configs extends readonly SprinklesProperties[]>(
     ...definePropsFn: Configs
-): SprinklesFn<Configs> => {
+): SprinklesFn<Configs> & {
+    conditions: SprinklesConditions<Configs>;
+    shorthands: Map<string, string[]>;
+} => {
     // console.log("createBoxSprinklesInternal");
     const original = createSprinkles(...definePropsFn);
     const config = getSprinklesConfig(definePropsFn);
