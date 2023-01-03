1e2f5556479844b84daf49fab277746e1f769462
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-01-03 10:26:09 +0100

    docs: addMappedPropsToUsedMap

--- packages/vanilla-extract/src/plugins/addMappedPropsToUsedMap.ts
+++ packages/vanilla-extract/src/plugins/addMappedPropsToUsedMap.ts
@@ -1,5 +1,37 @@
-import { UsedComponentMap } from "./getUsedPropertiesFromExtractNodeMap";
+import type { UsedComponentMap } from "./getUsedPropertiesFromExtractNodeMap";
 
+/**
+ * Let's say you have a component that uses one (or multiple) mapped prop:
+ *
+ * ```tsx
+ * const Stack = ({ direction = "column", spacing = 5 }) => {
+    return (
+        <div
+            className={sprinklesFn({
+                flexDirection: direction,
+                paddingRight: direction === "row" ? spacing : undefined,
+                paddingBottom: direction === "column" ? spacing : undefined,
+            })}
+        />
+    );
+};
+```
+ *
+ * And you use it like this:
+ *
+ * ```tsx
+ * <Stack direction="row" spacing="8">
+ * // children
+ * </Stack>
+ * ```
+ *
+ * The `direction` prop will be mapped to `flexDirection` implicitly in the component.
+ * But the @box-extractor/vanilla-extract plugin will only associate the `direction` prop with the value `row` and not with the `flexDirection` prop.
+ * Same goes for the `spacing` prop, which will be mapped to `paddingRight` and `paddingBottom` implicitly but only `spacing` prop will only be associated with the `8` value.
+ *
+ * This functions solves that with the plugin option `mappedProps: { direction: ["flexDirection"], space: ["paddingBottom", "paddingRight"] }`
+ * So that the `flexDirection` and `paddingRight`/`paddingBottom` props will also be associated with the `row` and `8` values respectively.
+ */
 export function addMappedPropsToUsedMap(mappedProps: Record<string, string[]>, usedMap: UsedComponentMap) {
     const mapped = mappedProps ?? {};
     usedMap.forEach((el, _componentName) => {
