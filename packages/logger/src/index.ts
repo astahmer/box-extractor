7a80739e3bb6f52f0f946e9a2aea43072baa98cc
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-01-09 11:25:46 +0100

    refactor(logger): use debug colors + timeFrom by vite

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,31 +1,16 @@
 // adapted from https://github.com/maraisr/diary since it doesn't support ESM
+// mixed with debug colors
+
 import pc from "picocolors";
 import util from "node:util";
 
 util.inspect.defaultOptions.depth = 6;
 
-const possibleColors = [
-    "red",
-    "green",
-    "yellow",
-    "blue",
-    "magenta",
-    "cyan",
-    "bgRed",
-    "bgGreen",
-    "bgYellow",
-    "bgBlue",
-    "bgMagenta",
-    "bgCyan",
-    "bgWhite",
-] as const;
-const possibleStyles = ["bold", "dim", "italic", "underline", "inverse"] as const;
-
 export type LogEvent = {
     name: string;
     level: LogLevels;
     messages: unknown[];
-    colorFn: (msg: string) => string;
+    color: number;
 };
 
 type LogLevels = "fatal" | "error" | "warn" | "debug" | "info" | "log";
@@ -86,40 +71,57 @@ const logger = (
     name: string,
     reporter: Reporter,
     level: LogLevels,
-    colorFn: (msg: string) => string,
+    color: LogEvent["color"],
     ...messages: unknown[]
 ): void => {
-    reporter({ name, level, colorFn, messages });
+    reporter({ name, level, color, messages });
+};
+
+/**
+ * taken from debug
+ * https://github.com/debug-js/debug/blob/d1616622e4d404863c5a98443f755b4006e971dc/src/browser.js#L27
+ */
+function selectColor(namespace: string): number {
+    let hash = 0;
+
+    for (let i = 0; i < namespace.length; i++) {
+        hash = (hash << 5) - hash + (namespace.codePointAt(i) ?? 0);
+        hash = Math.trunc(hash); // Convert to 32bit integer
+    }
+
+    return colors[Math.abs(hash) % colors.length]!;
+}
+
+const colors = [
+    20, 21, 26, 27, 32, 33, 38, 39, 40, 41, 42, 43, 44, 45, 56, 57, 62, 63, 68, 69, 74, 75, 76, 77, 78, 79, 80, 81, 92,
+    93, 98, 99, 112, 113, 128, 129, 134, 135, 148, 149, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172,
+    173, 178, 179, 184, 185, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 214, 215, 220, 221,
+];
+
+const withColor = (color: LogEvent["color"], str: string) => {
+    const colorCode = "\u001B[3" + (color < 8 ? color : "8;5;" + color);
+    return `${colorCode};1m${str} \u001B[0m`;
 };
 
 // ~ Reporter
 
 export const default_reporter: Reporter = (event) => {
     const fn = console[event.level === "fatal" ? "error" : event.level];
-    return void fn(event.colorFn(pc.bold(event.name)), ...event.messages);
-};
+    const color = event.color;
 
-const isOdd = (x: number) => x % 2 !== 0;
+    fn(pc.bold(withColor(color, event.name)), ...event.messages);
+};
 
 // ~ Public api
 
-let refCount = 0;
 const scopedCache = new Map<string, CreateLoggerReturn>();
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 const noop = () => {};
 
 export function createLogger(name: string, _onEmit?: Reporter): CreateLoggerReturn {
     const onEmit = _onEmit ?? default_reporter;
-
-    const color = possibleColors[refCount % possibleColors.length]!;
-    const style = isOdd(Math.floor(refCount / possibleStyles.length))
-        ? possibleStyles[refCount % possibleStyles.length]!
-        : undefined;
-    const styleFn = style ? pc[style] : undefined;
-    const colorFn = (msg: string) => (styleFn ? styleFn(pc[color](msg)) : pc[color](msg));
-    refCount++;
-
-    const logFn = isEnabled(name) ? logger.bind(0, name, onEmit, "debug", colorFn) : noop;
+    const color = selectColor(name);
+    const logFn = isEnabled(name) ? logger.bind(0, name, onEmit, "debug", color) : noop;
 
     return Object.assign(logFn, {
         extend: (scoped: string) => createLogger(`${name}:${scoped}`, onEmit),
@@ -149,3 +151,40 @@ type CreateLoggerReturn = ((...args: unknown[]) => void) & {
     lazy: (fn: () => any) => void;
     lazyScoped: (scoped: string, fn: () => any) => void;
 };
+
+/**
+ * taken from vite
+ * https://github.com/vitejs/vite/blob/167753d3754507430600a1bc2b100ca321b17a86/packages/vite/src/node/utils.ts#L360
+ */
+export function timeFrom(start: number, subtract = 0): string {
+    const time: number | string = performance.now() - start - subtract;
+    const timeString = (time.toFixed(2) + "ms").padEnd(5, " ");
+    if (time < 10) {
+        return pc.green(timeString);
+    } else if (time < 50) {
+        return pc.yellow(timeString);
+    } else {
+        return pc.red(timeString);
+    }
+}
+
+// const printColors = () => {
+//     for (let i = 0; i < 16; i++) {
+//         for (let j = 0; j < 16; j++) {
+//             const color = i * 16 + j;
+//             process.stdout.write(`\u001B[38;5;${color}m${String(color).padEnd(4)}\u001B[0m`);
+//         }
+
+//         console.log("");
+//     }
+
+//     for (let i = 0; i < 16; i++) {
+//         for (let j = 0; j < 16; j++) {
+//             const color = i * 16 + j;
+//             process.stdout.write(`\u001B[48;5;${color}m${String(color).padEnd(4)}\u001B[0m`);
+//         }
+
+//         console.log("");
+//     }
+
+// }
