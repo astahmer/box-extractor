9697ceaaa6c3ff155429b840b73af576caae7336
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-01-23 23:06:39 +0100

    fix: rm node protocol for preconstruct build

--- packages/box-extractor/src/extensions-helpers.ts
+++ packages/box-extractor/src/extensions-helpers.ts
@@ -1,4 +1,5 @@
-import { isAbsolute, resolve } from "node:path";
+// eslint-disable-next-line unicorn/import-style, unicorn/prefer-node-protocol
+import { isAbsolute, resolve } from "path";
 
 export const tsRE = /\.tsx?$/;
 export const ensureAbsolute = (path: string, root: string) =>
