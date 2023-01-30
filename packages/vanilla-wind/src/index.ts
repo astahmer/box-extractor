25470041e640fe1f9c46f36d91f30bb5a283ef72
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-01-30 10:45:17 +0100

    types(vw): export ConfigDynamicProperties, ConfigConditions

--- packages/vanilla-wind/src/index.ts
+++ packages/vanilla-wind/src/index.ts
@@ -1,1 +1,3 @@
 export { type GenericConfig, defineProperties } from "./defineProperties";
+// eslint-disable-next-line simple-import-sort/exports
+export type { ConfigDynamicProperties, ConfigConditions } from "./types";
