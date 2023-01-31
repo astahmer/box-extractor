553f64c7a0a0056912fce43e6cb03d2ee25dfa88
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-01-31 10:07:36 +0100

    wip(vw): components extraction with <Box /> PoC

--- packages/vanilla-wind/src/index.ts
+++ packages/vanilla-wind/src/index.ts
@@ -1,3 +1,4 @@
+export type { ThemeProps, TReturnFn, WithStyledProps } from "./defineProperties";
 export { type GenericConfig, defineProperties } from "./defineProperties";
 // eslint-disable-next-line simple-import-sort/exports
 export type { ConfigDynamicProperties, ConfigConditions } from "./types";
