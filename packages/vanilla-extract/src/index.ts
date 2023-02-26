c780a7b532856b87ac518298287d5f2309a64701
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-02-26 10:57:13 +0100

    chore: rm deprecated packages/vanilla-extract

--- packages/vanilla-extract/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { createBoxSprinkles } from "./createBoxSprinkles";
-export { createBoxSprinklesInternal } from "./createBoxSprinklesInternal";
-export { composeClassNames, getBoxProps } from "./getBoxProps";
-export type { SprinkleConditionsNames } from "./sprinkle-types";
-export type { AnySprinklesFn } from "./types";
